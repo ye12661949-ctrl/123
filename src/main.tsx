83ccd6ddb962cd74a639b31ddb3f4f481dd71f0e
ye12661949-ctrl <ye12661949@gmail.com>
2026-09-18 19:02:12 +0800

    Load directory and MoMA research supplements

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './directorySupplement';
+import './momaSupplement';
 import App from './App';
 import './index.css';
 
