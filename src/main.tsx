ddd747bd30bbe1c528fd4072960676bec3bec17a
ye12661949-ctrl <ye12661949@gmail.com>
2026-09-18 19:07:07 +0800

    Load The Photographers' Gallery supplement

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './directorySupplement';
 import './momaSupplement';
+import './tpgSupplement';
 import App from './App';
 import './index.css';
 
